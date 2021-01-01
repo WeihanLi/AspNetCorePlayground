c8e7ad7ad08016e7ef9404c912ff3e179e7b1c52
weihanli <weihanli@outlook.com>
2021-01-01 11:21:25 +0800

    add OcelotDemo.csproj

---
+++ OcelotDemo/OcelotMiddleware/UrlBasedAuthenticationMiddleware.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Ocelot.Authorization;
+using Ocelot.Logging;
+using Ocelot.Middleware;
+
+namespace OcelotDemo.OcelotMiddleware
+{
+    public class ApiPermission
+    {
+        public string AllowedRoles { get; set; }
+
+        public string PathPattern { get; set; }
+
+        public string Method { get; set; }
+    }
+
+    public class UrlBasedAuthenticationMiddleware : Ocelot.Middleware.OcelotMiddleware
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMemoryCache _memoryCache;
+        private readonly RequestDelegate _next;
+
+        public UrlBasedAuthenticationMiddleware(RequestDelegate next, IConfiguration configuration, IMemoryCache memoryCache, IOcelotLoggerFactory loggerFactory) : base(loggerFactory.CreateLogger<UrlBasedAuthenticationMiddleware>())
+        {
+            _next = next;
+
+            _configuration = configuration;
+            _memoryCache = memoryCache;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            // var permissions = await _memoryCache.GetOrCreateAsync("ApiPermissions", async entry =>
+            //{
+            //    using (var conn = new SqlConnection(_configuration.GetConnectionString("ApiPermissions")))
+            //    {
+            //        entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(1);
+            //        return (await conn.QueryAsync<ApiPermission>("SELECT * FROM dbo.ApiPermissions")).ToArray();
+            //    }
+            //});
+
+            var permissions = new[]
+            {
+               new ApiPermission()
+               {
+                   PathPattern = "/api/test/values",
+                   Method = "GET",
+                   AllowedRoles = ""
+               },
+               new ApiPermission()
+               {
+                   PathPattern = "/api/test/user",
+                   Method = "GET",
+                   AllowedRoles = "User"
+               },
+               new ApiPermission()
+               {
+                   PathPattern = "/api/test/admin",
+                   Method = "GET",
+                   AllowedRoles = "Admin"
+               },
+            };
+
+            var downstreamRoute = httpContext.Items.DownstreamRoute();
+
+            var result = await httpContext.AuthenticateAsync(downstreamRoute.AuthenticationOptions.AuthenticationProviderKey);
+            if (result.Principal != null)
+            {
+                httpContext.User = result.Principal;
+            }
+
+            var user = httpContext.User;
+            var request = httpContext.Request;
+
+            var permission = permissions.FirstOrDefault(p =>
+                request.Path.ToString().Equals(p.PathPattern, StringComparison.OrdinalIgnoreCase) && p.Method.ToUpper() == request.Method.ToUpper());
+
+            if (permission == null)// 完全匹配不到，再根据正则匹配
+            {
+                permission =
+                    permissions.FirstOrDefault(p =>
+                        Regex.IsMatch(request.Path.ToString(), p.PathPattern, RegexOptions.IgnoreCase) && p.Method.ToUpper() == request.Method.ToUpper());
+            }
+
+            if (user.Identity?.IsAuthenticated == true)
+            {
+                if (!string.IsNullOrWhiteSpace(permission?.AllowedRoles) &&
+                    !permission.AllowedRoles.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                        .Any(r => user.IsInRole(r)))
+                {
+                    httpContext.Items.SetError(new UnauthorizedError("forbidden, have no permission"));
+                    return;
+                }
+            }
+            else
+            {
+                if (permission != null && string.IsNullOrWhiteSpace(permission.AllowedRoles))
+                {
+                }
+                else
+                {
+                    httpContext.Items.SetError(new UnauthenticatedError("unauthorized, need login"));
+                    return;
+                }
+            }
+
+            await _next.Invoke(httpContext);
+        }
+    }
+}
---
+++ OcelotDemo/Startup.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.Cache.Middleware;
+using Ocelot.DependencyInjection;
+using Ocelot.DownstreamRouteFinder.Middleware;
+using Ocelot.DownstreamUrlCreator.Middleware;
+using Ocelot.Errors.Middleware;
+using Ocelot.LoadBalancer.Middleware;
+using Ocelot.Middleware;
+using Ocelot.Multiplexer;
+using Ocelot.Request.Middleware;
+using Ocelot.Requester.Middleware;
+using Ocelot.RequestId.Middleware;
+using Ocelot.Responder.Middleware;
+using OcelotDemo.OcelotMiddleware;
+using WeihanLi.Web.Authentication;
+using WeihanLi.Web.Authentication.QueryAuthentication;
+
+namespace OcelotDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(QueryAuthenticationDefaults.AuthenticationSchema)
+                .AddQuery();
+            services.AddOcelot();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseOcelot((ocelotBuilder, ocelotConfiguration) =>
+            {
+                // this sets up the downstream context and gets the config
+                app.UseDownstreamContextMiddleware();
+                // This is registered to catch any global exceptions that are not handled
+                // It also sets the Request Id if anything is set globally
+                ocelotBuilder.UseExceptionHandlerMiddleware();
+                // This is registered first so it can catch any errors and issue an appropriate response
+                ocelotBuilder.UseResponderMiddleware();
+                ocelotBuilder.UseDownstreamRouteFinderMiddleware();
+                ocelotBuilder.UseMultiplexingMiddleware();
+                ocelotBuilder.UseDownstreamRequestInitialiser();
+                ocelotBuilder.UseRequestIdMiddleware();
+
+                ocelotBuilder.UseMiddleware<UrlBasedAuthenticationMiddleware>();
+                ocelotBuilder.UseLoadBalancingMiddleware();
+                ocelotBuilder.UseDownstreamUrlCreatorMiddleware();
+                ocelotBuilder.UseOutputCacheMiddleware();
+                ocelotBuilder.UseHttpRequesterMiddleware();
+            }).Wait();
+        }
+    }
+}
